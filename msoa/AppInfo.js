dd96bf1c3ecd32ff38f105edd22a4ab169540842
bhsiao2 <baichun.hsiao@dxc.com>
2025-06-03 11:41:16 +0800

    render layout with vue data add uat msoa cert

---
+++ msoa/AppInfo.js
@@ -0,0 +1,45 @@
+const App =  {
+    data(){
+        return {
+            appName: "聯卡e客服",
+            datas :[
+                {
+                    supSystem: 'Android',
+                    env: 'UAT',
+                    appVersion: 'version 1.3.3',
+                    checkValue: '20250602',
+                    ticketNumber: 'M2025057_R114087',
+                    changeLog: '憑證更新',
+                    download: 'https://drive.google.com/file/d/11ONAIFNPvE9LXMuumThYKIXF6njq6b_d/view?usp=sharing',
+                },
+                {
+                    supSystem: 'iOS',
+                    env: 'UAT',
+                    appVersion: 'version 1.5.9',
+                    checkValue: '20250602',
+                    ticketNumber: 'M2025057_R114087',
+                    changeLog: '憑證更新',
+                    download: 'itms-services://?action=download-manifest&url=https://dxcncccteam.github.io/msoa/manifest.plist',
+                },
+                {
+                    supSystem: 'Android',
+                    env: 'UAT',
+                    appVersion: 'version 1.3.2',
+                    checkValue: '20241203',
+                    ticketNumber: 'M2024159_R113307',
+                    changeLog: 'FCM API升級',
+                    download: 'https://drive.google.com/file/d/1moZaroDAKAI49vc42Wkaw8FV6-fs9nCC/view?usp=sharing',
+                },
+                {
+                    supSystem: 'iOS',
+                    env: 'UAT',
+                    appVersion: 'version 1.5.8',
+                    checkValue: '20250422',
+                    ticketNumber: 'M2024190_R113369',
+                    changeLog: 'Xcode 16升級',
+                    download: 'itms-services://?action=download-manifest&url=https://dxcncccteam.github.io/msoa/manifest-uat.plist',
+                },
+            ]
+        }
+    }
+}
