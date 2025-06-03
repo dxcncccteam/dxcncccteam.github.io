076734de608252c8737bb463c9ab41d491f0afbf
dxc <dxcncccteam@gmail.com>
2025-06-03 09:59:41 +0800

    render layout with vue data

---
+++ CrossApp/AppInfo.js
@@ -0,0 +1,16 @@
+const App =  {
+        data(){
+            return {
+                datas :[
+                    {
+                        AppName: 'CrossApp',
+                        SupSystem: 'Android',
+                        env: '',
+                        AppVersion: 'version 1.1',
+                        ticketNumber: '',
+                        changeLog: '密碼可輸入英文',
+                        download: 'https://drive.google.com/file/d/1QjhIx7XctioJwxViXfHEfb075ty9wxKW/view?usp=drive_link',
+                    }]
+            }
+        }
+    }
